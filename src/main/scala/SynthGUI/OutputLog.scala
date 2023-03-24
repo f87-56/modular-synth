0dc7cfc6349b203236030ca8fab1a7bd00b26386
f87-56 <45468114+f87-56@users.noreply.github.com>
2023-03-24 21:42:46 +0200

    Added logging functionality

---
+++ src/main/scala/SynthGUI/OutputLog.scala
@@ -0,0 +1,28 @@
+package SynthGUI
+import scala.collection.mutable
+import scala.collection.mutable.Buffer
+
+// a log to output info to the user
+object OutputLog {
+
+  val LOG_SIZE = 256
+  val listeners:mutable.Buffer[LogListener] = mutable.Buffer()
+
+  val history:mutable.Queue[String] = mutable.Queue()
+
+  def log(s:String) = 
+    if(history.size >= LOG_SIZE) then
+      history.dequeue()
+      history += s
+    else
+      history += s
+
+  def lastLog = history.last
+  
+  def wholeLog = history.toVector
+}
+
+trait LogListener:
+  OutputLog.listeners += this
+  
+  def onNewMessage:Unit
