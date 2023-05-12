9ee68e8c4baeb06c93bed329a2e77a55216e9bab
f87-56 <45468114+f87-56@users.noreply.github.com>
2023-05-12 13:00:38 +0300

    Added error handling to audio resource access

--- src/main/scala/SynthSoundIO/SynthRuntime.scala
+++ src/main/scala/SynthSoundIO/SynthRuntime.scala
@@ -1,4 +1,5 @@
 package SynthSoundIO
+import SynthGUI.OutputLog
 import SynthLogic.ModularSynthesizer
 import SynthSoundIO.SynthRuntime.{BIT_DEPTH, BIT_RATE, BYTE_BUFFER_SIZE, SYNTH_BUFFER_SIZE}
 import SynthUtilities.MathUtilities
@@ -9,7 +10,7 @@ import javax.sound.midi.{MidiDevice, MidiMessage, Receiver, ShortMessage, Transm
 import javax.sound.sampled.{AudioFormat, AudioSystem, DataLine, SourceDataLine}
 import scala.collection.mutable
 import scala.concurrent.Future
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 /**
  * The "Execution ground" for modular synthesizers. Handles passing information between the sound system and modular synthesizers.
@@ -23,16 +24,27 @@ class SynthRuntime extends Receiver:
 
   private val messageQueue:mutable.Queue[MidiMessage] = mutable.Queue()
 
-  // TODO: Error handling
-  // TODO: Remove magic constants
-  // TODO: Configurability
-  // Hopefully temporary. We just need to send audio now.
   val format = new AudioFormat(BIT_RATE, BIT_DEPTH, 1, true, false)
   //val info = new DataLine.Info(classOf[SourceDataLine], format)
   // format.issupported etc. etc.
-  val line:SourceDataLine = AudioSystem.getSourceDataLine(format)
-  line.open(format, SynthRuntime.OUT_BUFFER_SIZE)
-  line.start()
+  val line:Option[SourceDataLine] = {
+    Try(AudioSystem.getSourceDataLine(format)) match
+      case Failure(exception) =>
+        System.err.println("Could not get an audio data line")
+        OutputLog.log("Could not get an audio data line")
+        None
+      case Success(value) =>
+        Some(value)
+  }
+  Try {
+    line.foreach(a =>
+      a.open(format, SynthRuntime.OUT_BUFFER_SIZE)
+      a.start())
+  } match
+    case Failure(exception) =>
+      System.err.println("Could not open audio data line")
+      OutputLog.log("Could not open audio data line")
+    case Success(value) => ()
 
   private var kill = false
 
@@ -43,7 +55,7 @@ class SynthRuntime extends Receiver:
 
     def writeToAudio(): Unit =
       val data = buildOutput(Try(Some(messageQueue.dequeue())).getOrElse(None))
-      line.write(data, 0, BYTE_BUFFER_SIZE)
+      line.foreach(_.write(data, 0, BYTE_BUFFER_SIZE))
 
     val a = LazyList.continually(writeToAudio())
     Future(a.takeWhile(* => !kill).foreach(_ => ()))
@@ -51,10 +63,10 @@ class SynthRuntime extends Receiver:
   private def closeOutput(): Unit =
     kill = true
 
-  def buildOutput(shortMessage:Option[MidiMessage]):Array[Byte] =
+  def buildOutput(shortMessage: Option[MidiMessage]): Array[Byte] =
     Array.tabulate(SYNTH_BUFFER_SIZE)(
-      _ => (MathUtilities.clamp(-1,1, activeSynth.output(shortMessage))
-        *Short.MaxValue).toShort)
+      _ => (MathUtilities.clamp(-1, 1, activeSynth.output(shortMessage))
+        * Short.MaxValue).toShort)
       .flatMap(a => MathUtilities.breakToBytes(a))
 
   /*
@@ -63,14 +75,16 @@ class SynthRuntime extends Receiver:
    */
   def send(msg: MidiMessage, timestamp: Long): Unit =
     val status = msg.getStatus
-    if(status == ShortMessage.NOTE_ON ||status == ShortMessage.NOTE_OFF) then
+    if (status == ShortMessage.NOTE_ON || status == ShortMessage.NOTE_OFF) then
       messageQueue += msg
 
   // Stops output and frees up audio resources
   def close(): Unit =
     closeOutput()
-    line.stop()
-    line.close()
+    line.foreach(a =>
+      a.stop()
+      a.close()
+    )
 
 end SynthRuntime
 object SynthRuntime:
